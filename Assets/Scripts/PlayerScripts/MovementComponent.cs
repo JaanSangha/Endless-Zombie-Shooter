a0a66d36c39b871256051bfe863ec016523dbddb
JaanSangha <jaansangha10@gmail.com>
2022-01-24 23:07:01 -0500

    Week 2 Lab completed

--- Assets/Scripts/PlayerScripts/MovementComponent.cs
+++ Assets/Scripts/PlayerScripts/MovementComponent.cs
@@ -16,13 +16,22 @@ public class MovementComponent : MonoBehaviour
     //components
     private PlayerController playerController;
     Rigidbody rigidbody;
+    Animator playerAnimator;
 
     //references
     Vector2 inputVector = Vector2.zero;
     Vector3 moveDirection = Vector3.zero;
 
+    //animator hashes
+    public readonly int movementXHash = Animator.StringToHash("MovementX");
+    public readonly int movementYHash = Animator.StringToHash("MovementY");
+    public readonly int isJumpingHash = Animator.StringToHash("IsJumping");
+    public readonly int isRunningHash = Animator.StringToHash("IsRunning");
+
+
     private void Awake()
     {
+        playerAnimator = GetComponent<Animator>();
         rigidbody = GetComponent<Rigidbody>();
         playerController = GetComponent<PlayerController>();
     }
@@ -49,16 +58,20 @@ public class MovementComponent : MonoBehaviour
     public void OnMovement(InputValue value)
     {
         inputVector = value.Get<Vector2>();
+        playerAnimator.SetFloat(movementXHash, inputVector.x);
+        playerAnimator.SetFloat(movementYHash, inputVector.y);
     }
 
     public void OnRun(InputValue value)
     {
         playerController.isRunning = value.isPressed;
+        playerAnimator.SetBool(isRunningHash, playerController.isRunning);
     }
 
     public void OnJump(InputValue value)
     {
         playerController.isJumping = value.isPressed;
+        playerAnimator.SetBool(isJumpingHash, playerController.isJumping);
         rigidbody.AddForce((transform.up + moveDirection) * jumpForce, ForceMode.Impulse);
     }
 
@@ -67,6 +80,6 @@ public class MovementComponent : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground") && !playerController.isJumping) return;
         
         playerController.isJumping = false;
-      
+        playerAnimator.SetBool(isJumpingHash, false);
     }
 }
